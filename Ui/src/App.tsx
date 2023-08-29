cb3dbd7feec731b83b4439ac39ce43e832a8c5d8
Matas Razumas <matas.razumas0@gmail.com>
2023-08-29 20:58:11 +0300

    initial

---
+++ Ui/src/App.tsx
@@ -0,0 +1,9 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+import { FC } from "react";
+
+const App: FC = () => {
+  return <div>Game Content Management System</div>;
+};
+
+export default App;
